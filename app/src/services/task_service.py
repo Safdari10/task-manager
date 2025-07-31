dc20c7347d50aec50b803519a3fba5780fc80612
Safdari10 <zakersafdari@gmail.com>
2025-07-31 17:12:10 +1200

    Add update_task method to TaskService for task updates

--- app/src/services/task_service.py
+++ app/src/services/task_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from typing import Optional
-from app.src.schemas.schemas import TaskResponse, TaskCreate
+from app.src.schemas.schemas import TaskResponse, TaskCreate, TaskUpdate
 from app.src.repositories.task_repository import TaskRepository
 
 
@@ -16,3 +16,11 @@ class TaskService:
 
     def create_task(self, task_create: TaskCreate) -> TaskResponse:
         return self.task_repository.create_task(task_create)
+
+    def update_task(
+        self, task_id: int, task_update: TaskUpdate
+    ) -> Optional[TaskResponse]:
+        task = self.task_repository.update_task(task_id, task_update)
+        if not task:
+            return None
+        return task
