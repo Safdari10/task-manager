416025e73cb8d7f1b6b804550d6b80965cb24925
Safdari10 <zakersafdari@gmail.com>
2025-07-31 23:12:01 +1200

    Add helper function to create test tasks to allow for testing other endpoints

--- tests/test_task.py
+++ tests/test_task.py
@@ -2,11 +2,26 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from fastapi.testclient import TestClient
 from app.main import app
 from app.src.schemas.schemas import TaskResponse
 
+# initialize the test client
 client = TestClient(app)
+
+
+def create_test_task():
+    test_task = {
+        "title": "Test Task",
+        "description": "This is a test task",
+        "status": "pending",
+    }
+    response = client.post("/tasks", json=test_task)  # type: ignore
+    assert response.status_code in (200, 201)  # Accept either 200 or 201
+    data = response.json()  # type: ignore
+    TaskResponse.model_validate(data)
+    return data["id"]  #  Return the created task ID to be used in required tests
 
 
 def test_get_all_tasks():
