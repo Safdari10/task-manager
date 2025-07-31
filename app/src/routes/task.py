a84f9a689faba211bfb4bf1aa0100d47a9989dbf
Safdari10 <zakersafdari@gmail.com>
2025-07-31 17:42:24 +1200

    Add update_task endpoint for updating a task by ID

--- app/src/routes/task.py
+++ app/src/routes/task.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.src.db.db import get_db_session
 from app.src.services.task_service import TaskService
-from task_manager.app.src.schemas.schemas import TaskResponse
+from task_manager.app.src.schemas.schemas import TaskResponse, TaskUpdate
 
 
 router = APIRouter()
@@ -15,6 +15,19 @@ def get_task(
 ):
     service = TaskService(db)
     task = service.get_task(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@router.put("/tasks/{task_id}", response_model=TaskResponse, tags=["Update a task"])
+def update_task(
+    task_id: int,
+    task_update: TaskUpdate,
+    db: Session = Depends(get_db_session),
+):
+    service = TaskService(db)
+    task = service.update_task(task_id, task_update)
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
