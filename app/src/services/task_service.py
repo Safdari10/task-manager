758e6b2cf41c5d8f44a1accc6f5f1188489563a4
Safdari10 <zakersafdari@gmail.com>
2025-07-31 17:06:40 +1200

    Implement get_task method for task retrieval

--- app/src/services/task_service.py
+++ app/src/services/task_service.py
@@ -1,7 +1,15 @@
 from sqlalchemy.orm import Session
+from typing import Optional
+from app.src.schemas.schemas import TaskResponse
 from app.src.repositories.task_repository import TaskRepository
 
 
 class TaskService:
     def __init__(self, db: Session) -> None:
-        self.task_respository = TaskRepository(db)
+        self.task_repository = TaskRepository(db)
+
+    def get_task(self, task_id: int) -> Optional[TaskResponse]:
+        task = self.task_repository.get_task(task_id)
+        if not task:
+            return None
+        return task
