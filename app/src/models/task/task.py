725ab89b2c827c08676b988d59e69e738af157e0
Safdari10 <zakersafdari@gmail.com>
2025-08-01 20:42:02 +1200

    Refactor Task model: add user_id column with foreign key relationship to users

--- app/src/models/task/task.py
+++ app/src/models/task/task.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+from sqlalchemy.orm import relationship
 from app.src.db.db import Base, utcnow
 
 
@@ -9,6 +10,9 @@ from app.src.db.db import Base, utcnow
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    user_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id"), index=True, nullable=False
+    )
     title = Column(String(length=255), index=True)
     description = Column(
         String(length=1000), nullable=True
@@ -21,6 +25,7 @@ class Task(Base):
         default=utcnow,
         onupdate=utcnow,
     )
+    user = relationship("User", back_populates="tasks")
 
 
 # index = true means that the column will be indexed for faster search
