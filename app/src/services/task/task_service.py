302d0f13ba6926c0c62f94ec5b4d2184d7c7afa4
Safdari10 <zakersafdari@gmail.com>
2025-08-01 21:30:55 +1200

    Refactor TaskService methods to include user_id for task operations

--- app/src/services/task/task_service.py
+++ app/src/services/task/task_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from typing import Optional
+from pydantic import UUID4
 from app.src.schemas.task.schemas import (
     TaskResponse,
     TaskCreate,
@@ -12,28 +13,19 @@ class TaskService:
     def __init__(self, db: Session) -> None:
         self.task_repository = TaskRepository(db)
 
-    def get_all_tasks(self) -> list[TaskResponse]:
-        return self.task_repository.get_all_tasks()
+    def get_all_tasks(self, user_id: UUID4) -> list[TaskResponse]:
+        return self.task_repository.get_all_tasks(user_id)
 
-    def get_task(self, task_id: int) -> Optional[TaskResponse]:
-        task = self.task_repository.get_task(task_id)
-        if not task:
-            return None
-        return task
+    def get_task(self, task_id: UUID4, user_id: UUID4) -> Optional[TaskResponse]:
+        return self.task_repository.get_task(task_id, user_id)
 
-    def create_task(self, task_create: TaskCreate) -> TaskResponse:
-        return self.task_repository.create_task(task_create)
+    def create_task(self, user_id: UUID4, task_create: TaskCreate) -> TaskResponse:
+        return self.task_repository.create_task(user_id, task_create)
 
     def update_task(
-        self, task_id: int, task_update: TaskUpdate
+        self, task_id: UUID4, user_id: UUID4, task_update: TaskUpdate
     ) -> Optional[TaskResponse]:
-        task = self.task_repository.update_task(task_id, task_update)
-        if not task:
-            return None
-        return task
+        return self.task_repository.update_task(task_id, user_id, task_update)
 
-    def delete_task(self, task_id: int) -> Optional[TaskResponse]:
-        task = self.task_repository.delete_task(task_id)
-        if not task:
-            return None
-        return task
+    def delete_task(self, task_id: UUID4, user_id: UUID4) -> Optional[TaskResponse]:
+        return self.task_repository.delete_task(task_id, user_id)
