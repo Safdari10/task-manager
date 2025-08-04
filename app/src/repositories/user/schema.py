0dc5a352b0643520abe93022467125a16e8aa6c5
Safdari10 <zakersafdari@gmail.com>
2025-08-04 15:56:30 +1200

    Adds user login retrieval by email
    Introduces a method to fetch a user by email address, enabling login functionality and supporting user authentication features.

--- app/src/repositories/user/schema.py
+++ app/src/repositories/user/schema.py
@@ -1,6 +1,14 @@
 from sqlalchemy.orm import Session
+from typing import Optional
+from app.src.models.user.user import User
 
 
 class UserRepository:
     def __init__(self, db: Session) -> None:
         self.db = db
+
+    def login_user(self, email: str) -> Optional[User]:
+        user = self.db.query(User).filter(User.email == email).first()
+        if user:
+            return user
+        return None
