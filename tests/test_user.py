21f8ae2e9a0bc12be41f788104b5c8395c50c56b
Safdari10 <zakersafdari@gmail.com>
2025-08-06 14:44:04 +1200

    Adds FastAPI TestClient setup for API testing
    Integrates TestClient to enable direct API endpoint testing
    and prepares the test environment for future test cases.
    Facilitates more effective and automated validation of API logic.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,10 @@
 import os
 import sys
-
+from fastapi.testclient import TestClient
+from app.main import app
 
 # add the parent directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+client = TestClient(app)
