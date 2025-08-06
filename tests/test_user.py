c287a6720b09307bf46f96574ee993b013413f74
Safdari10 <zakersafdari@gmail.com>
2025-08-06 15:29:12 +1200

    Adds user login test and refines type hints
    Introduces a new test for user login to verify authentication flow
    and updates type annotations for improved clarity and consistency
    in user registration tests.

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,7 +2,7 @@ import os
 import sys
 from fastapi.testclient import TestClient
 from app.main import app
-from app.src.schemas.user_schemas import UserResponse
+from app.src.schemas.user_schemas import UserResponse, UserLoginResponse
 from tests.conftest import generate_unique_email
 
 # add the parent directory to the system path
@@ -12,9 +12,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 client = TestClient(app)
 
 
+def test_user_login(create_user: dict[str, str]):
+    login_data: dict[str, str] = {
+        "email": create_user["email"],
+        "password": create_user["password"],
+    }
+    response = client.post("/login", json=login_data)  # type: ignore
+    assert response.status_code == 200
+    data = response.json()  # type: ignore
+    UserLoginResponse.model_validate(data)
+
+
 def test_register_user():
     email = generate_unique_email()
-    user_data = {  # type: ignore
+    user_data: dict[str, str] = {
         "first_name": "Test",
         "last_name": "User",
         "email": email,
