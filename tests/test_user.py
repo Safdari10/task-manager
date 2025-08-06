4842e65b0d384e708e1069bdd0d8f253fb60db8f
Safdari10 <zakersafdari@gmail.com>
2025-08-06 14:56:27 +1200

    Adds user registration API test
    Introduces automated test for user registration endpoint
    to ensure correct response structure and status code.
    Promotes reliability and coverage of user creation flow.

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,9 +2,27 @@ import os
 import sys
 from fastapi.testclient import TestClient
 from app.main import app
+from app.src.schemas.user_schemas import UserResponse
+from tests.conftest import generate_unique_email
 
 # add the parent directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 client = TestClient(app)
+
+
+def test_register_user():
+    email = generate_unique_email()
+    user_data = {  # type: ignore
+        "first_name": "Test",
+        "last_name": "User",
+        "email": email,
+        "password": "Test@123",
+    }
+    response = client.post("/register", json=user_data)  # type: ignore
+    assert response.status_code == 201
+    data = response.json()  # type: ignore
+    UserResponse.model_validate(data)
+    assert data["email"] == email
+    assert "id" in data
