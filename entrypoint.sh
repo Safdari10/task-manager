8192d74aa426b3479df9deb4ce8c2b47d6a9b0e9
Safdari10 <zakersafdari@gmail.com>
2025-08-12 17:23:53 +1200

    Adds entrypoint script for container startup
    Introduces a startup script to ensure the database is ready before applying migrations and launching the application, improving reliability during container initialization.

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Entrypoint script for Docker container, we need this because we want to ensure that the database is ready before starting the application.
+
+# Wait for the PostgreSQL database to be available
+echo "Waiting for db to be ready..."
+/app/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Database is up"
+
+# Run Alembic migrations
+echo "Running Alembic migrations..."
+alembic upgrade head
+
+# Start FastAPI app with Uvicorn
+echo "Starting FastAPI app..."
+exec uvicorn app.src.main:app --host 0.0.0.0 --port 8000
