1075436864278e77302d422818cc96a2784f8008
Safdari10 <zakersafdari@gmail.com>
2025-08-19 13:53:06 +1200

    Adds state handling for email and password inputs
    Enables controlled components for authentication fields, allowing real-time input tracking and future validation or submission logic.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <main className="flex flex-col items-center justify-center bg-[#c9def0] w-full h-[100vh] p-16 rounded-2xl shadow-md text-center">
@@ -10,11 +15,21 @@ const Home = () => {
           <div className="flex flex-col items-center justify-between mt-8">
             <label htmlFor="email" className="w-64 text-sm font-medium text-gray-600">
               EMAIL ADDRESS
-              <input type="email" className="block mt-2 w-full p-1 border-b-1 border-b-gray-400" />
+              <input
+                type="email"
+                className="block mt-2 w-full p-1 border-b-1 border-b-gray-400"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
             <label htmlFor="password" className="w-64 mt-8 text-sm font-medium text-gray-600">
               PASSWORD
-              <input type="text" className="block mt-2 w-full p-1 border-b-1 border-b-gray-400" />
+              <input
+                type="text"
+                className="block mt-2 w-full p-1 border-b-1 border-b-gray-400"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </label>
           </div>
           <button
