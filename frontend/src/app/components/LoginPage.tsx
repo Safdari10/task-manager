86bdfe535b0a4f75bb568098060edf6a37702818
Safdari10 <zakersafdari@gmail.com>
2025-08-19 20:14:53 +1200

    Updates router import to use next/navigation
    Switches routing import to the recommended module for Next.js app directory compatibility and improved navigation handling.

--- frontend/src/app/components/LoginPage.tsx
+++ frontend/src/app/components/LoginPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { login } from "../hooks/useLogin";
 import { LoginProps } from "../types/loginSignup";
 
--- frontend/src/app/components/SignupPage.tsx
+++ frontend/src/app/components/SignupPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { signup } from "../hooks/useSignUp";
 import { SignUpProps } from "../types/loginSignup";
 
