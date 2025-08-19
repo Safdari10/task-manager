7bf0b1b00d03382c9fca9fb701e0323a3f5d27bc
Safdari10 <zakersafdari@gmail.com>
2025-08-19 18:59:00 +1200

    Refines login/signup UI for improved clarity and style
    Updates typography, spacing, button styles, and capitalization
    to enhance visual hierarchy and consistency between login and
    signup toggles. Improves user experience and aligns with
    modern UI practices.

--- frontend/src/app/components/LoginSignupToggle.tsx
+++ frontend/src/app/components/LoginSignupToggle.tsx
@@ -7,25 +7,25 @@ const LoginSignupToggle = ({ display, setDisplay }: LoginSignupToggleProps) => {
   return (
     <>
       {display === "login" ? (
-        <div className="flex flex-col items-center justify-center gap-60 p-14 w-[40%] h-[100vh] bg-gray-700 ">
+        <div className="flex flex-col items-center justify-center gap-56 p-14 w-[40%] h-[100vh] bg-gray-700 ">
           <div className="flex flex-col items-center gap-4">
-            <h1 className="text-3xl font-bold text-blue-500">New here?</h1>
-            <p className="text-white">sign up and discover</p>
+            <h1 className="text-3xl font-bold text-blue-600">New here?</h1>
+            <p className="text-lg text-white">Sign up and discover</p>
           </div>
           <button
-            className="bg-blue-500 text-white py-2 px-4 rounded"
+            className="w-full bg-blue-500 text-lg text-white py-2 px-4 rounded-3xl hover:bg-blue-600 transition-colors duration-300"
             onClick={() => setDisplay("signup")}>
             Sign Up
           </button>
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center gap-60 p-14 w-[40%] h-[100vh] bg-gray-700 ">
+        <div className="flex flex-col items-center justify-center gap-56 p-14 w-[40%] h-[100vh] bg-gray-700 ">
           <div className="flex flex-col items-center gap-4">
-            <h1 className="text-2xl font-bold text-blue-600">One of us?</h1>
-            <p className="text-white">just sign in</p>
+            <h1 className="text-3xl font-bold text-blue-600">One of us?</h1>
+            <p className="text-lg text-white">Just sign in</p>
           </div>
           <button
-            className="bg-blue-500 text-white py-2 px-4 rounded"
+            className="w-full bg-blue-500 text-lg text-white py-2 px-4 rounded-3xl hover:bg-blue-600 transition-colors duration-300"
             onClick={() => setDisplay("login")}>
             Sign In
           </button>
