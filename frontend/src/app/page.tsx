0d2bb1605f17ca39112426e6978759f9ce164c5d
Safdari10 <zakersafdari@gmail.com>
2025-08-21 19:16:53 +1200

    Improves layout with centered container and shadow
    Enhances the authentication page's visual appeal by centering content within a fixed-size, shadowed container and adding vertical padding. Improves user experience by making the login and signup forms more prominent and visually distinct.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,31 +14,33 @@ const Home = () => {
   const [display, setDisplay] = useState("login");
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      {display === "login" ? (
-        <LoginPage
-          email={email}
-          setEmail={setEmail}
-          password={password}
-          setPassword={setPassword}
-          error={error}
-          setError={setError}
-        />
-      ) : (
-        <SignupPage
-          firstName={firstName}
-          setFirstName={setFirstName}
-          lastName={lastName}
-          setLastName={setLastName}
-          email={email}
-          setEmail={setEmail}
-          password={password}
-          setPassword={setPassword}
-          error={error}
-          setError={setError}
-        />
-      )}
-      <LoginSignupToggle display={display} setDisplay={setDisplay} />
+    <div className="flex items-center justify-center w-full max-h-screen py-96">
+      <div className="w-[1024px] h-[700px] flex items-center justify-center shadow-lg">
+        {display === "login" ? (
+          <LoginPage
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            error={error}
+            setError={setError}
+          />
+        ) : (
+          <SignupPage
+            firstName={firstName}
+            setFirstName={setFirstName}
+            lastName={lastName}
+            setLastName={setLastName}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            error={error}
+            setError={setError}
+          />
+        )}
+        <LoginSignupToggle display={display} setDisplay={setDisplay} />
+      </div>
     </div>
   );
 };
