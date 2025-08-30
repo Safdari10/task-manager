64a79087633df9e086a1e55f7e625ce818b757b1
Safdari10 <zakersafdari@gmail.com>
2025-08-30 21:24:10 +1200

    Improves top bar styling for better usability
    Refines search input and button appearance by enhancing spacing,
    rounded corners, and background contrast. Aims to provide a more
    visually appealing and user-friendly interface.

--- frontend/src/app/dashboard/components/TopBar.tsx
+++ frontend/src/app/dashboard/components/TopBar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const TopBar = () => {
   return (
-    <div className="flex items-center justify-between w-full p-4 bg-white rounded-2xl  shadow-md">
-      <input type="text" placeholder="Search tasks..." />
+    <div className="flex items-center justify-between w-full p-4 bg-white rounded-3xl  shadow-md">
+      <input
+        type="text"
+        placeholder="Search tasks..."
+        className="w-2/6 bg-gray-200 rounded-lg p-2 text-md"
+      />
       <div>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded">+ New Task</button>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg">+ New Task</button>
       </div>
     </div>
   );
