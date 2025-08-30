ec7b4c6e4ce181a879627b1dda1d25677be5205d
Safdari10 <zakersafdari@gmail.com>
2025-08-30 21:38:00 +1200

    Pass setOpenTab prop to SidePanel component in TaskManager

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -12,7 +12,7 @@ const TaskManager = () => {
   return (
     <div className="flex items-start justify-center w-full h-full bg-gray-100">
       <div className="w-1/4 h-screen py-10 pl-10">
-        <SidePanel />
+        <SidePanel setOpenTab={setOpenTab} />
       </div>
       <main className="w-3/4 h-full flex flex-col p-10 gap-6">
         <TopBar />
