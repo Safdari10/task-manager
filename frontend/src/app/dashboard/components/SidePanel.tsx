65972249cf95434bcda695d0e334d5b6dd0c8b75
Safdari10 <zakersafdari@gmail.com>
2025-08-30 22:30:24 +1200

    Add border to buttons in SidePanel for improved visual separation

--- frontend/src/app/dashboard/components/SidePanel.tsx
+++ frontend/src/app/dashboard/components/SidePanel.tsx
@@ -9,21 +9,21 @@ const SidePanel = ({ openTab, setOpenTab }: SidePanelProps) => {
   return (
     <div className="flex flex-col items-center justify-between w-full h-full py-2 bg-white rounded-3xl shadow-md">
       <div className="flex flex-col items-center justify-center w-full pt-4">
-        <div className="flex flex-col items-center justify-center w-full pb-6">
+        <div className="flex flex-col items-center justify-center w-full pb-6 border-b-2 border-b-gray-200">
           <h1 className="w-full text-left text-2xl font-bold text-blue-600 px-6">Task Manager</h1>
           <p className="w-full text-left mt-4 text-gray-600 px-6">Manage your tasks efficiently</p>
         </div>
         <div className="flex flex-col items-center justify-center w-full">
           <button
             onClick={() => setOpenTab("dashboard")}
-            className={`w-full text-left py-3 px-6 cursor-pointer ${
+            className={`w-full text-left py-3 px-6 cursor-pointer border-b-2 border-b-gray-200  ${
               openTab === "dashboard" ? "bg-amber-100" : ""
             }`}>
             Dashboard
           </button>
           <button
             onClick={() => setOpenTab("myTasks")}
-            className={`w-full text-left py-3 px-6 cursor-pointer ${
+            className={`w-full text-left py-3 px-6 cursor-pointer border-b-2 border-b-gray-200  ${
               openTab === "myTasks" ? "bg-amber-100" : ""
             }`}>
             My Tasks
