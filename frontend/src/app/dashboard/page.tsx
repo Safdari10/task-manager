fd5d446d70edb8f66558501ba8b67b56c920a564
Safdari10 <zakersafdari@gmail.com>
2025-08-30 22:01:44 +1200

    Improves dashboard layout for better alignment
    Refactors dashboard structure to enhance centering and spacing,
    updates background colors, and adjusts side panel and main content
    widths for improved visual consistency and user experience.

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -10,15 +10,17 @@ const TaskManager = () => {
   const [openTab, setOpenTab] = useState("dashboard");
 
   return (
-    <div className="flex items-start justify-center w-full h-full bg-gray-100">
-      <div className="w-1/4 h-screen py-10 pl-10">
-        <SidePanel setOpenTab={setOpenTab} />
+    <div className="flex items-center justify-center w-full h-full bg-gray-100">
+      <div className="flex items-start justify-center w-full h-full max-w-[1280px] bg-amber-50 ">
+        <div className="w-[20%] h-screen py-10 pl-10">
+          <SidePanel setOpenTab={setOpenTab} />
+        </div>
+        <main className="w-[80%] h-full flex flex-col p-10 gap-6">
+          <TopBar />
+          {openTab === "dashboard" && <Dashboard />}
+          {openTab === "myTasks" && <MyTasks />}
+        </main>
       </div>
-      <main className="w-3/4 h-full flex flex-col p-10 gap-6">
-        <TopBar />
-        {openTab === "dashboard" && <Dashboard />}
-        {openTab === "myTasks" && <MyTasks />}
-      </main>
     </div>
   );
 };
