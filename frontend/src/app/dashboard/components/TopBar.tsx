06742b7fe7bcabde26f08bd4d709e459fbd88bf9
Safdari10 <zakersafdari@gmail.com>
2025-08-30 21:04:27 +1200

    Adds rounded corners to top bar for improved UI
    Enhances the visual appearance of the dashboard's top bar
    by introducing rounded corners, creating a softer and more
    modern look that aligns with current design trends.

--- frontend/src/app/dashboard/components/TopBar.tsx
+++ frontend/src/app/dashboard/components/TopBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TopBar = () => {
   return (
-    <div className="flex items-center justify-between w-full p-4 bg-white shadow-md">
+    <div className="flex items-center justify-between w-full p-4 bg-white rounded-2xl  shadow-md">
       <input type="text" placeholder="Search tasks..." />
       <div>
         <button className="bg-blue-500 text-white py-2 px-4 rounded">+ New Task</button>
