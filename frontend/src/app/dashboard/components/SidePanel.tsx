ab5fe0587a2f5e2b253056293a17eb86f54cbc9f
Safdari10 <zakersafdari@gmail.com>
2025-08-30 21:53:55 +1200

    Adjust padding in SidePanel for improved layout consistency

--- frontend/src/app/dashboard/components/SidePanel.tsx
+++ frontend/src/app/dashboard/components/SidePanel.tsx
@@ -6,9 +6,9 @@ interface SidePanelProps {
 
 const SidePanel = ({ setOpenTab }: SidePanelProps) => {
   return (
-    <div className="flex flex-col items-start justify-between w-full h-full py-2 px-6 bg-white rounded-3xl shadow-md">
+    <div className="flex flex-col items-start justify-between w-full h-full py-2 bg-white rounded-3xl shadow-md">
       <div className="flex flex-col items-start justify-center gap-4 pt-4">
-        <div className="mb-6">
+        <div className="mb-6 px-6">
           <h1 className="text-2xl font-bold text-blue-600">Task Manager</h1>
           <p className="mt-4 text-gray-600">Manage your tasks efficiently</p>
         </div>
