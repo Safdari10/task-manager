98a54878708a2b3be67a4f85e5b23a999cd8f264
Safdari10 <zakersafdari@gmail.com>
2025-09-01 16:09:07 +1200

    Adjusts padding for calendar and task preview panels
    Refines vertical and horizontal padding on dashboard panels for more consistent spacing and improved visual alignment.

--- frontend/src/app/dashboard/components/Calender.tsx
+++ frontend/src/app/dashboard/components/Calender.tsx
@@ -46,7 +46,7 @@ const Calender = () => {
     setCurrentYear(newYear);
   };
   return (
-    <div className="w-[20rem] h-max-content flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md p-6">
+    <div className="w-[20rem] h-max-content flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4 px-6">
       <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4">
         <h2 className="text-lg font-semibold">
           {months[currentMonth]} {currentYear}
