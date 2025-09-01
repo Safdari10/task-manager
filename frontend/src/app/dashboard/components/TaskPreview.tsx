cede11646f90733cb3d5cef46ffe6aa9b0b0ce53
Safdari10 <zakersafdari@gmail.com>
2025-09-01 15:31:34 +1200

    Improves task preview styling and layout
    Enhances visual appearance by adding custom classes for spacing,
    background, rounding, and shadow, creating a more polished and
    user-friendly component.

--- frontend/src/app/dashboard/components/TaskPreview.tsx
+++ frontend/src/app/dashboard/components/TaskPreview.tsx
@@ -1,5 +1,9 @@
 const TaskPreview = () => {
-  return <div>TaskPreview</div>;
+  return (
+    <div className="w-[20rem] w-max-content flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md p-6">
+      TaskPreview
+    </div>
+  );
 };
 
 export default TaskPreview;
