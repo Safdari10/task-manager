b2999114b615d00d52474a5796042ef19d44128d
Safdari10 <zakersafdari@gmail.com>
2025-09-04 22:40:24 +1200

    Increases height of Calender and TaskPreview components for consistent layout

--- frontend/src/app/dashboard/components/Calender.tsx
+++ frontend/src/app/dashboard/components/Calender.tsx
@@ -44,7 +44,7 @@ const Calender = () => {
     setCurrentYear(newYear);
   };
   return (
-    <div className="w-[20rem] h-[22rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4 px-6">
+    <div className="w-[20rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4 px-6">
       <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4">
         <h2 className="text-md font-bold">
           {months[currentMonth]} {currentYear}
