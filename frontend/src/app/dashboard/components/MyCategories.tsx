0e34af14d4270fb8b19b1445bee686a14af1a7b6
Safdari10 <zakersafdari@gmail.com>
2025-09-05 23:12:00 +1200

    Enhances category header with icon and styling
    Improves the visual presentation of the category section
    by adding a styled header, title, and a menu icon for
    future actions. Increases clarity and aligns with UI
    design patterns.

--- frontend/src/app/dashboard/components/MyCategories.tsx
+++ frontend/src/app/dashboard/components/MyCategories.tsx
@@ -1,7 +1,14 @@
+import { FaEllipsisV } from "react-icons/fa";
+
 const MyCategories = () => {
   return (
     <div className="w-[20rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4">
-      MyCategories
+      <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4 px-6">
+        <h2 className="text-md font-bold">My Categories</h2>
+        <div className="text-gray-700 cursor-pointer pr-4">
+          <FaEllipsisV />
+        </div>
+      </div>
     </div>
   );
 };
