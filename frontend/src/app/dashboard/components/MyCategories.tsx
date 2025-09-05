f2d79336087a88c7df3faeadfb624452fa3832c4
Safdari10 <zakersafdari@gmail.com>
2025-09-05 23:10:23 +1200

    Enhances UI layout for categories panel
    Applies structured styling and layout to the categories panel
    to improve visual hierarchy and user experience.

--- frontend/src/app/dashboard/components/MyCategories.tsx
+++ frontend/src/app/dashboard/components/MyCategories.tsx
@@ -1,5 +1,9 @@
 const MyCategories = () => {
-  return <div>MyCategories</div>;
+  return (
+    <div className="w-[20rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4">
+      MyCategories
+    </div>
+  );
 };
 
 export default MyCategories;
