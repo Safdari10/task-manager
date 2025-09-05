c950a45bd63cbc5974985ab4198a31cbb1d727bc
Safdari10 <zakersafdari@gmail.com>
2025-09-05 23:17:34 +1200

    Adjusts calendar component width for improved layout

--- frontend/src/app/dashboard/components/Calender.tsx
+++ frontend/src/app/dashboard/components/Calender.tsx
@@ -44,7 +44,7 @@ const Calender = () => {
     setCurrentYear(newYear);
   };
   return (
-    <div className="w-[20rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4">
+    <div className="w-[18rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4">
       <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4 px-6">
         <h2 className="text-base font-bold">
           {months[currentMonth]} {currentYear}
