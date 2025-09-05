87cbe8632755740fe8b0aab9909e3830d38b01d8
Safdari10 <zakersafdari@gmail.com>
2025-09-05 23:22:58 +1200

    Improves comments section header styling
    Enhances the visual structure of the comments section by adding a styled header with bold text and a separating border, increasing clarity and visual appeal.

--- frontend/src/app/dashboard/components/Comments.tsx
+++ frontend/src/app/dashboard/components/Comments.tsx
@@ -1,7 +1,9 @@
 const Comments = () => {
   return (
     <div className="w-[10rem] h-[23rem] flex flex-col justify-start items-start gap-2 bg-white rounded-3xl shadow-md py-4">
-      New Comments
+      <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4 px-6">
+        <h2 className="text-md font-bold">New Comments</h2>
+      </div>
     </div>
   );
 };
