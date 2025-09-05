baa789389026fe43e4bbe15414544dd1ed1b1fc7
Safdari10 <zakersafdari@gmail.com>
2025-09-05 23:08:29 +1200

    Switches dashboard layout from flex to CSS grid
    Adopts a CSS grid layout with defined column widths to improve
    structure and alignment of dashboard components, allowing for
    more precise control over the UI appearance and adaptability.

--- frontend/src/app/dashboard/components/Dashboard.tsx
+++ frontend/src/app/dashboard/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import TaskPreview from "./TaskPreview";
 
 const Dashboard = () => {
   return (
-    <div className="w-full h-full flex items-start justify-start gap-10 ">
+    <div className="w-full h-full grid [grid-template-columns:20rem_25rem_15rem] gap-10 ">
       <Calender />
       <TaskPreview />
       <Comments />
