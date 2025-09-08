038ee0baff76ab77905dd2f70c1404e9999640a8
Safdari10 <zakersafdari@gmail.com>
2025-09-08 18:59:25 +1200

    Adds section header to task tracking panel
    Introduces a styled "My Tracking" header for improved clarity
    and visual separation of the task tracking section.

--- frontend/src/app/dashboard/components/TaskTracker.tsx
+++ frontend/src/app/dashboard/components/TaskTracker.tsx
@@ -1,7 +1,9 @@
 const TaskTracker = () => {
   return (
     <div className="w-[22rem] h-[23rem] flex flex-col items-start gap-2 bg-white rounded-3xl shadow-md py-4">
-      TaskTracker
+      <div className="flex items-center justify-between w-full border-b-2 border-b-gray-200 pb-4 px-6">
+        <h2 className="text-md font-bold">My Tracking</h2>
+      </div>
     </div>
   );
 };
