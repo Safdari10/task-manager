7344199233657a903d747b7b4c6c0d5f19bf0310
Safdari10 <zakersafdari@gmail.com>
2025-09-08 18:58:30 +1200

    Enhances task tracker with styled container
    Improves the visual structure of the task tracker by adding a styled container, making the component more visually appealing and better suited for future UI enhancements.

--- frontend/src/app/dashboard/components/TaskTracker.tsx
+++ frontend/src/app/dashboard/components/TaskTracker.tsx
@@ -1,5 +1,9 @@
 const TaskTracker = () => {
-  return <div>TaskTracker</div>;
+  return (
+    <div className="w-[22rem] h-[23rem] flex flex-col items-start gap-2 bg-white rounded-3xl shadow-md py-4">
+      TaskTracker
+    </div>
+  );
 };
 
 export default TaskTracker;
