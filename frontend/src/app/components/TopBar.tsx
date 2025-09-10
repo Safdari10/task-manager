16d826700a9774b11070699c641d7838781a60a8
Safdari10 <zakersafdari@gmail.com>
2025-09-10 23:13:08 +1200

    Adds modal for creating new tasks from top bar
    Integrates a modal component triggered by the "New Task" button,
    improving the task creation workflow and user experience.
    Enables users to initiate new tasks directly from the main navigation.

--- frontend/src/app/components/TopBar.tsx
+++ frontend/src/app/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaUserCircle, FaPlus } from "react-icons/fa";
+import CreateTask from "./CreateTask";
 
 const TopBar = () => {
   const [isCreateTaskModalOpen, setIsCreateTaskModalOpen] = useState(false);
@@ -9,21 +10,26 @@ const TopBar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between w-full p-4 bg-white rounded-3xl  shadow-md">
-      <input
-        type="text"
-        placeholder="Search tasks..."
-        className="w-2/6 bg-gray-200 rounded-lg p-2 text-md"
-      />
-      <div className="flex items-center">
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg flex items-center cursor-pointer">
-          <FaPlus className="mr-2" /> New Task
-        </button>
-        <button className="bg-amber-200 text-black p-2 rounded-full ml-4 cursor-pointer">
-          <FaUserCircle className="text-2xl" />
-        </button>
+    <>
+      <div className="flex items-center justify-between w-full p-4 bg-white rounded-3xl  shadow-md">
+        <input
+          type="text"
+          placeholder="Search tasks..."
+          className="w-2/6 bg-gray-200 rounded-lg p-2 text-md"
+        />
+        <div className="flex items-center">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-lg flex items-center cursor-pointer"
+            onClick={handleNewTaskClick}>
+            <FaPlus className="mr-2" /> New Task
+          </button>
+          <button className="bg-amber-200 text-black p-2 rounded-full ml-4 cursor-pointer">
+            <FaUserCircle className="text-2xl" />
+          </button>
+        </div>
       </div>
-    </div>
+      {isCreateTaskModalOpen && <CreateTask />}
+    </>
   );
 };
 
